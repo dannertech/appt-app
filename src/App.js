8147ca3af4ca79c3d736574e61d66829d41c0c9e
dannertech <danner@danner.tech>
2019-03-06 23:52:43 -0500

    styled header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 
 const StyledHeader = styled.div`
 text-align: center;
+background-color: rgb(10,186,181);
+border: 1px solid;
+padding: 10px;
+box-shadow: 5px 10px #E8E8E8;
+color: white;
+
 `
 
 class App extends Component {
   render() {
     return (
       <StyledHeader>
-        <h1>You are at the main page</h1>
+        <h1>Appt App</h1>
       </StyledHeader>
     );
   }
