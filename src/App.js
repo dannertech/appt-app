d62a7660c9bc22ae65d5a51053bdbd1876a131c6
dannertech <danner@danner.tech>
2019-03-07 00:03:19 -0500

    added subheader and styled it

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ border: 1px solid;
 padding: 10px;
 box-shadow: 5px 10px #E8E8E8;
 color: white;
+`
 
+const StyledSubheader = styled.div`
+text-align: center;
+padding: 20px 0px 0px 0px;
+color: rgb(10,186,181);
+text-decoration: underline;
 `
 
 class App extends Component {
   render() {
     return (
+      <div>
       <StyledHeader>
         <h1>Appt App</h1>
       </StyledHeader>
+      <StyledSubheader>
+<h3>Schedule Your Appointment</h3>
+      </StyledSubheader>
+      </div>
     );
   }
 }
